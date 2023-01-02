67e4f7adf167c6f6c6b2bac37f54e368f1540249
hakanbayraktarABC <121807819+hakanbayraktarABC@users.noreply.github.com>
2023-01-02 16:42:51 +0300

    Create project.lua

---
+++ project.lua
@@ -0,0 +1,53 @@
+local Finity = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/UI-Librarys/main/Finity%20UI%20Lib"))()
+
+local FHub = Finity.new(true)
+
+local CreditsCategory = FHub:Category("Credits")
+local MainCategory = FHub:Category("Main")
+
+-- Credits Sectors
+local CreditsDevelopers = CreditsCategory:Sector("Developers")
+local CreditsContact = CreditsCategory:Sector("Contact")
+local CreditsAboutUs = CreditsCategory:Sector("About Us")
+
+-- Main Sectors
+
+local MainAutos = MainCategory:Sector("Auto's")
+local MainTeleport = MainCategory:Sector("Teleport")
+
+-- Credits Sectors Developers Label
+local devLabel1 = CreditsDevelopers:Cheat("Label", "Farmex441#0001",
+    function() end, {})
+
+local devLabel2 = CreditsDevelopers:Cheat("Label", "Sallanan Kedi",
+    function() end, {})
+
+local devLabel3 = CreditsDevelopers:Cheat("Label", "HakanTelB21921921",
+    function() end, {})
+
+-- Credits Sectors Contact Label
+local contactLabel1 = CreditsContact:Cheat("Label", "Farmex441#0001",
+    function() end, {})
+
+local contactLabel2 = CreditsContact:Cheat("Label", "Sallanan Kedi",
+    function() end, {})
+
+local contactLabel3 = CreditsContact:Cheat("Label", "xxprooyuncuoyunda",
+    function() end, {})
+
+local contactLabel4 = CreditsContact:Cheat("Label", "haydarpro.baba@gmail.com - hakanbayraktar30@abc.k12.tr",
+    function() end, {})
+
+-- Credits Sectors About Us Label
+
+local aboutUsLabel1 = CreditsContact:Cheat("Label", "I am from Turkiye.",
+    function() end, {})
+
+local aboutUsLabel2 = CreditsContact:Cheat("Label", "I'll make a basic roblox scripts.",
+    function() end, {})
+
+local aboutUsLabel3 = CreditsContact:Cheat("Label", "I know Deutsch and English.",
+    function() end, {})
+
+local aboutUsLabel4 = CreditsContact:Cheat("Label", "I'll help everyone in my discord server.",
+    function() end, {})
